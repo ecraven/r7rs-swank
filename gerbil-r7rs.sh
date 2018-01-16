e4a2244d86b3fc48b3a868ca8a2b40487ee81127
Peter <craven@gmx.net>
2018-01-16 15:12:31 +0100

    Only set GERBIL_LOADPATH if it isn't set already

--- gerbil-r7rs.sh
+++ gerbil-r7rs.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-export GERBIL_LOADPATH=.
+dir=$(dirname "$0")
+export GERBIL_LOADPATH=${GERBIL_LOADPATH:-$dir}
 gxi --lang r7rs -e '(import (ecraven gerbil-swank))' -e '(start-swank "/tmp/gerbil-swank.txt")'
