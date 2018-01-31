a0e3c6e6c0c4346c3ac28b572f5213f3c182e463
Peter <craven@gmx.net>
2018-01-31 14:20:20 +0100

    remove obsolete LD_LIBRARY_PATH

--- chez.sh
+++ chez.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-export LD_LIBRARY_PATH=/home/nex/scheme/chez/arcfide/
 echo '(import (chez-swank)) (start-swank "/tmp/chez-swank.port")' | chez-scheme --libdirs '.:/home/nex/scheme/chez'
 #chez-scheme --libdirs .:/home/nex/scheme/chez
