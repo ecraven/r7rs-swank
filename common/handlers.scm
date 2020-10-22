13c23b21c18a1640d14c655e817be32a548cae3b
Peter <craven@gmx.net>
2020-10-22 08:44:23 +0200

    Bump version to 2.26.

--- common/handlers.scm
+++ common/handlers.scm
@@ -25,7 +25,7 @@
          :features (:swank)
          :modules ("SWANK-ARGLISTS" "SWANK-REPL" "SWANK-PRESENTATIONS")
          :package (:name "(user)" :prompt "(user)")
-         :version "2.24"))
+         :version "2.26"))
 
 (define-slime-handler (swank:swank-require packages)
   '())
